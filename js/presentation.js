b9f8315d8fe0f97700006010144f11000c8e72b7
Alan Palazzolo <github@zzolo.org>
2010-08-08 17:31:42 +0200

    Initial commit of Presentation for Mapping in Drupal with OpenLayers for DrupalCon Copenhagen 2010.

---
+++ js/presentation.js
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+$(document).ready(function(){
+  $('#slides').presentation({
+    slide: '.slide', //Reference to each individual slide
+    pagerClass: 'nav-pager', //Class to put on the unordered list that contains links to each slide
+    prevNextClass: 'nav-prev-next', //Class to put on the unordered list that contains the previous and next links
+    prevText: 'Previous', //Text for the Previous link
+    nextText: 'Next', //Text for the Next link
+    transition: 'fade'
+  });
+});
