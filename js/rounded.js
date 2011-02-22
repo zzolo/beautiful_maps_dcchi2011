b718bcd0efc80b555451ba8a83bddbf218f4fc0f
Alan Palazzolo <github@zzolo.org>
2011-02-21 18:16:38 -0800

    Updated general styling and image to into slide.

--- js/rounded.js
+++ js/rounded.js
@@ -2,7 +2,7 @@
  *
  */
 $(document).ready(function() {
-  var round = '5px';
+  var round = '3px';
   $('.demo-links a').corner(round);
   $('.nav-prev-next a').corner('bottom ' + round);
   $('.nav-pager a').corner('top ' + round);
